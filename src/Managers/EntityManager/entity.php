ed31bb4d3682cd8b2e1f27b8ad7e4f7086cba21a
Demko Igor <i.demko1@yandex.ru>
2016-03-30 10:31:23 +0300

    - Добавлен менеджер сущностей. - Подключен Pimple.

---
+++ src/Managers/EntityManager/entity.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Author: Demko Igor
+ */
+
+namespace Entity;
+
+
+use Interop\Container\ContainerInterface;
+
+class Entity {
+
+	public function __construct($container = []) {
+		if (is_array($container)) {
+			$container = new Container($container);
+		}
+
+		if (!$container instanceof ContainerInterface) {
+			throw new \InvalidArgumentException('Expected a ContainerInterface');
+		}
+
+		$this->container = $container;
+	}
+
+	public function createTable($tableName, $fields) {
+		$fields[$tableName . '_unique_id'] = function($value) {
+			return $value;
+		};
+
+
+		$sql = '';
+
+	}
+
+}
